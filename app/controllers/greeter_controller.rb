a3f050b0c526fedf9d38f32e6c13e999a2adc052
Kalman Hazins <kalmanh@gmail.com>
2014-06-01 11:33:24 -0400

    About to integrate code with HTTParty...

--- app/controllers/greeter_controller.rb
+++ app/controllers/greeter_controller.rb
@@ -1,2 +1,9 @@
 class GreeterController < ApplicationController
+  def hello
+    random_names = %w{Joe Alex Harry} 
+    @name = random_names.sample
+    @time = Time.now
+    @times_displayed ||= 0
+    @times_displayed += 1
+  end
 end
